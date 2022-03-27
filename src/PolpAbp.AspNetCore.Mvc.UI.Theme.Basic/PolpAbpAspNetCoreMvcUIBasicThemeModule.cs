c3699cb1e0c70c5beaf2d6ffa6c7f4bb30e59838
X. Tang <x.long@infobeyondtech.com>
2022-03-27 10:50:07 -0400

    Factored here

---
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/PolpAbpAspNetCoreMvcUIBasicThemeModule.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.MultiTenancy;
+using PolpAbp.AspNetCore.Mvc.UI.Theme.Basic.Bundling;
+using PolpAbp.AspNetCore.Mvc.UI.Theme.Basic.Toolbars;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Toolbars;
+using Volo.Abp.AspNetCore.Mvc.UI.Theming;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Basic
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreMvcUiThemeSharedModule),
+        typeof(AbpAspNetCoreMvcUiMultiTenancyModule)
+        )]
+    public class PolpAbpAspNetCoreMvcUIBasicThemeModule : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            PreConfigure<IMvcBuilder>(mvcBuilder =>
+            {
+                mvcBuilder.AddApplicationPartIfNotExists(typeof(PolpAbpAspNetCoreMvcUIBasicThemeModule).Assembly);
+            });
+        }
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpThemingOptions>(options =>
+            {
+                options.Themes.Add<BasicTheme>();
+
+                if (options.DefaultThemeName == null)
+                {
+                    options.DefaultThemeName = BasicTheme.Name;
+                }
+            });
+
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<PolpAbpAspNetCoreMvcUIBasicThemeModule>("PolpAbp.AspNetCore.Mvc.UI.Theme.Basic");
+            });
+
+            Configure<AbpToolbarOptions>(options =>
+            {
+                options.Contributors.Add(new BasicThemeMainTopToolbarContributor());
+            });
+
+            Configure<AbpBundlingOptions>(options =>
+            {
+                options
+                    .StyleBundles
+                    .Add(BasicThemeBundles.Styles.Global, bundle =>
+                    {
+                        bundle
+                            .AddBaseBundles(StandardBundles.Styles.Global)
+                            .AddContributors(typeof(BasicThemeGlobalStyleContributor));
+                    });
+
+                options
+                    .ScriptBundles
+                    .Add(BasicThemeBundles.Scripts.Global, bundle =>
+                    {
+                        bundle
+                            .AddBaseBundles(StandardBundles.Scripts.Global)
+                            .AddContributors(typeof(BasicThemeGlobalScriptContributor));
+                    });
+            });
+        }
+    }
+}
