ed26ca7d20e5294bcb336a6d884aea8551edcd53
Tom Tang <polpware@gmail.com>
2022-08-11 04:29:55 -0400

    Introduce account tenancy component

---
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/AccountTenancy/AccountTenancyViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Basic.Themes.Basic.Components.AccountTenancy
+{
+    public class AccountTenancyViewComponent : AbpViewComponent
+    {
+        public virtual IViewComponentResult Invoke()
+        {
+            return View("~/Themes/Basic/Components/AccountTenancy/Default.cshtml");
+        }
+    }
+}
