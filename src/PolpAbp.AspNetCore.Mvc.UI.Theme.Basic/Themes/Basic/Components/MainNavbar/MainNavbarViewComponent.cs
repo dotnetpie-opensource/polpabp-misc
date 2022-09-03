516cad133feca53ca2f29a0c37e0c73969ed7553
Tom Tang <polpware@gmail.com>
2022-09-03 17:55:36 -0400

    Generalize the main nav to work for account page

--- src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/MainNavbar/MainNavbarViewComponent.cs
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/MainNavbar/MainNavbarViewComponent.cs
@@ -5,8 +5,18 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Basic.Themes.Basic.Components.MainNavb
 
 public class MainNavbarViewComponent : AbpViewComponent
 {
-    public virtual IViewComponentResult Invoke()
+    public virtual IViewComponentResult Invoke(bool isLight = false)
     {
-        return View("~/Themes/Basic/Components/MainNavbar/Default.cshtml");
+        var data = new ViewModel
+        {
+            IsLightBackground = isLight
+        };
+
+        return View("~/Themes/Basic/Components/MainNavbar/Default.cshtml", data);
+    }
+
+    public class ViewModel
+    {
+        public bool IsLightBackground { get; set; }
     }
 }
