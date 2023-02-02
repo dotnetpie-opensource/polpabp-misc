2ef2137367fb84c1e616727ed24c12b2a8cfa299
Tom <polpware@gmail.com>
2023-02-02 15:23:01 -0500

    Generalize the layout to support more variants

--- src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/AccountTenancy/AccountTenancyViewComponent.cs
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/AccountTenancy/AccountTenancyViewComponent.cs
@@ -5,10 +5,11 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Themes.Basic.Components.AccountTenancy
 {
     public class AccountTenancyViewComponent : AbpViewComponent
     {
-        public virtual IViewComponentResult Invoke(bool showResetBtn = false)
+        public virtual IViewComponentResult Invoke(bool showResetBtn = false, bool hidden = false)
         {
             return View("~/Themes/Basic/Components/AccountTenancy/Default.cshtml", new Model
             {
+                Hidden= hidden,
                 ShowResetButton = showResetBtn
             });
         }
--- src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/AccountTenancy/Model.cs
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/AccountTenancy/Model.cs
@@ -2,6 +2,7 @@
 {
     public class Model
     {
+        public bool Hidden { get; set; }
         public bool ShowResetButton { get; set; }
     }
 }
