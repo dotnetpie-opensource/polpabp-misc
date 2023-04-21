1b623573504d8e99c99577884c1401355eeed76b
dotnetpie <tom@fake.com>
2023-04-21 11:33:07 -0400

    Bootslander theme started

---
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander/Themes/Bootslander/Components/AccountTenancy/Model.cs
@@ -0,0 +1,8 @@
+﻿namespace PolpAbp.AspNetCore.Mvc.UI.Themes.Basic.Components.AccountTenancy
+{
+    public class Model
+    {
+        public bool Hidden { get; set; }
+        public bool ShowResetButton { get; set; }
+    }
+}
---
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander/Themes/Bootslander/Components/MainNavbar/MainNavbarViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace PolpAbp.AspNetCore.Mvc.UI.Themes.Basic.Components.MainNavbar;
+
+public class MainNavbarViewComponent : AbpViewComponent
+{
+    public virtual IViewComponentResult Invoke(bool isLight = false)
+    {
+        var data = new ViewModel
+        {
+            IsLightBackground = isLight
+        };
+
+        return View("~/Themes/Basic/Components/MainNavbar/Default.cshtml", data);
+    }
+
+    public class ViewModel
+    {
+        public bool IsLightBackground { get; set; }
+    }
+}
