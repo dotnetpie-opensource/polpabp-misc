c429481166c698eb36c9cb3cfa50f9b0c079de75
Tom Tang <polpware@gmail.com>
2023-04-26 05:26:46 -0400

    Work on Bootslander theme

---
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander/Themes/Bootslander/Components/Brand/MainNavbarBrandViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander.Themes.Bootslander.Components.Brand;
+
+public class MainNavbarBrandViewComponent : AbpViewComponent
+{
+    public virtual IViewComponentResult Invoke()
+    {
+        return View("~/Themes/Bootslander/Components/Brand/Default.cshtml");
+    }
+}
