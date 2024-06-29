9c886d0df4b8af54f3c3aee8a48dccbc0eba1d5a
Tom T <polpware@gmail.com>
2024-06-29 04:32:41 -0400

    Refined the features of the visitor theme: No user menu

--- src/PolpAbp.AspNetCore.Mvc.UI.Theme.Visitor/Toolbars/VisitorThemeMainTopToolbarContributor.cs
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Visitor/Toolbars/VisitorThemeMainTopToolbarContributor.cs
@@ -1,10 +1,8 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using PolpAbp.AspNetCore.Mvc.UI.Themes.Visitor.Components.Toolbar.LanguageSwitch;
-using PolpAbp.AspNetCore.Mvc.UI.Themes.Visitor.Components.Toolbar.UserMenu;
+using System.Threading.Tasks;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Toolbars;
 using Volo.Abp.Localization;
-using Volo.Abp.Users;
 
 namespace PolpAbp.AspNetCore.Mvc.UI.Themes.Visitor.Toolbars
 {
@@ -29,11 +27,6 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Themes.Visitor.Toolbars
             if (languages.Count > 1)
             {
                 context.Toolbar.Items.Add(new ToolbarItem(typeof(LanguageSwitchViewComponent)));
-            }
-
-            if (context.ServiceProvider.GetRequiredService<ICurrentUser>().IsAuthenticated)
-            {
-                context.Toolbar.Items.Add(new ToolbarItem(typeof(UserMenuViewComponent)));
             }
         }
     }
