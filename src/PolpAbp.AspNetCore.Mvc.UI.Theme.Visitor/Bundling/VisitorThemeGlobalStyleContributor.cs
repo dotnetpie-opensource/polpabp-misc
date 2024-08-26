deddf2256288550c30c539b6879b61e16bd082de
Tom T <polpware@gmail.com>
2024-08-26 06:15:26 -0400

    Switch to use custom themes

--- src/PolpAbp.AspNetCore.Mvc.UI.Theme.Visitor/Bundling/VisitorThemeGlobalStyleContributor.cs
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Visitor/Bundling/VisitorThemeGlobalStyleContributor.cs
@@ -1,4 +1,5 @@
 ﻿using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using Volo.Abp.Localization;
 
 namespace PolpAbp.AspNetCore.Mvc.UI.Bundling
 {
@@ -6,6 +7,23 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
+            var stdOnes = context.Files.Find(x => x.FileName.Contains("bootstrap.css"));
+            if (stdOnes != null)
+            {
+                context.Files.Remove(stdOnes);
+            }
+
+            if (CultureHelper.IsRtl)
+            {
+                // todo: Support RTL 
+                // context.Files.Add("/custom/bootstrap5/css/bootstrap.rtl.css");
+                context.Files.Add("/custom/bootstrap5/css/bootstrap.css");
+            }
+            else
+            {
+                context.Files.Add("/custom/bootstrap5/css/bootstrap.css");
+            }
+
             context.Files.Add("/themes/visitor/layout.css");
         }
     }
