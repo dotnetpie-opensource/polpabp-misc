023c78ec73e428106fb73b205429ec48a85d62a1
Tom T <polpware@gmail.com>
2024-08-28 04:27:59 -0400

    RTL for bootsland

--- src/PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander/Bundling/BootslanderThemeGlobalStyleContributor.cs
+++ src/PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander/Bundling/BootslanderThemeGlobalStyleContributor.cs
@@ -10,9 +10,7 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander.Bundling
 
             if (CultureHelper.IsRtl)
             {
-                // todo: Support RTL 
-                // context.Files.Add("/custom/bootstrap5/css/bootstrap.rtl.css");
-                context.Files.Add("/custom/bootstrap5/css/bootstrap.css");
+                context.Files.Add("/custom/bootstrap5/css/bootstrap.rtl.css");
             }
             else
             {
@@ -20,9 +18,8 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander.Bundling
             }
 
             context.Files.Add("/libs/animate-css/animate.css");
+            // todo: Maybe remove this
             context.Files.Add("/libs/bootstrap-icons/font/bootstrap-icons.css");
-            //context.Files.Add("/libs/boxicons/css/boxicons.css");
-            //context.Files.Add("/libs/remixicon/fonts/remixicon.css");
 
             context.Files.Add("/libs/line-awesome/line-awesome/css/line-awesome.css");
 
@@ -35,7 +32,14 @@ namespace PolpAbp.AspNetCore.Mvc.UI.Theme.Bootslander.Bundling
             // Theme-specific 
             context.Files.Add("/themes/bootslander/spacing.css");
 
-            context.Files.Add("/custom/bootslander/css/theme.css");
+            if (CultureHelper.IsRtl)
+            {
+                context.Files.Add("/custom/bootslander/css/theme.rtl.css");
+            }
+            else
+            {
+                context.Files.Add("/custom/bootslander/css/theme.css");
+            }
         }
     }
 }
